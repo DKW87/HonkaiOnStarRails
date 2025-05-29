928cc0930f8091ea0a656ae1afa20c7b2ee10b9e
DKW87 <dannykwant@gmail.com>
2025-05-29 14:05:43 +0200

    chore(import): removes unused

--- src/main/java/com/github/dkw87/honkaionstarrails/HonkaiOnStarRailsLauncher.java
+++ src/main/java/com/github/dkw87/honkaionstarrails/HonkaiOnStarRailsLauncher.java
@@ -7,7 +7,6 @@ import com.github.dkw87.honkaionstarrails.service.KeyInputService;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
