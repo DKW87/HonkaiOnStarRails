ae1c953aa1124fc0e67b2a8e7154f82e8f6fad06
DKW87 <dannykwant@gmail.com>
2025-05-31 21:17:21 +0200

    feat(CombatOffsets): addresses wil now work off of 1 BASE address instead of a hard address for everything

--- src/main/java/com/github/dkw87/honkaionstarrails/service/constant/CombatOffsets.java
+++ src/main/java/com/github/dkw87/honkaionstarrails/service/constant/CombatOffsets.java
@@ -5,15 +5,16 @@ public class CombatOffsets {
 
     public static final String GAME_ASSEMBLY_MODULE = "GameAssembly.dll";
 
-    public static final long IS_COMBAT_STARTING = 0x43AFD00L;
-    public static final long IS_COMBAT_INITIALIZING = 0x43AE3D0L;
-    public static final long IS_COMBAT_INITIALIZED = 0x43ADDA8L;
+    public static final long BASE = 0x43AFD00L;
 
-    public static final long IS_COMBAT_PAUSED = 0x43AE40FL;
+    public static final long IS_COMBAT_STARTING = BASE;
+    public static final long IS_COMBAT_INITIALIZING = BASE - 0x1930L;
+    public static final long IS_COMBAT_INITIALIZED = BASE - 0x1F58L;
 
-    public static final long IS_COMBAT_VIEW_OPEN = 0x43AE40EL;
+    public static final long IS_COMBAT_PAUSED = BASE - 0x18F1L;
+    public static final long IS_COMBAT_VIEW_OPEN = BASE - 0x18F2L;
 
-    public static final long TURN_COUNTER = 0x43B2B94L;
+    public static final long TURN_COUNTER = BASE + 0x2E94L;
 
     // not stable
     public static final long SKILLPOINTS_BASE = 0x4272180L;
