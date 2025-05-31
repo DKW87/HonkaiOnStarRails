b29698120bcde4e035a62f7e193037ecf3c821c5
DKW87 <dannykwant@gmail.com>
2025-05-31 15:02:24 +0200

    feat(DevUtil): adds method to log thread info

--- src/main/java/com/github/dkw87/honkaionstarrails/shared/utility/dev/DevUtil.java
+++ src/main/java/com/github/dkw87/honkaionstarrails/shared/utility/dev/DevUtil.java
@@ -5,10 +5,24 @@ import org.slf4j.LoggerFactory;
 
 import java.awt.MouseInfo;
 import java.awt.Point;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class DevUtil {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DevUtil.class);
+
+    public void readActiveThreads() {
+        Set<Thread> threads = Thread.getAllStackTraces().keySet();
+        LOGGER.debug("Active threads ({}): {}",
+                threads.size(),
+                threads.stream()
+                        .map(t -> String.format("%s (daemon: %s)", t.getName(), t.isDaemon()))
+                        .sorted()
+                        .collect(Collectors.joining(", "))
+        );
+    }
+
 
     public static void showMousePosition() throws InterruptedException {
         Thread.sleep(2500);
