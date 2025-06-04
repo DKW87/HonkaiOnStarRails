a1e58a864331a860eddcff278be2d442d7a0ba03
DKW87 <dannykwant@gmail.com>
2025-06-04 19:29:36 +0200

    feat(offset): adds inCombat

--- src/main/java/com/github/dkw87/honkaionstarrails/service/constant/CombatOffsets.java
+++ src/main/java/com/github/dkw87/honkaionstarrails/service/constant/CombatOffsets.java
@@ -7,14 +7,14 @@ public class CombatOffsets {
     public static final long ANCHOR = 0x43B4588L;
 
     // need to be updated start
-    public static final long IS_COMBAT_STARTING = ANCHOR;
-    public static final long IS_COMBAT_INITIALIZING = ANCHOR - 0x1930L;
-    public static final long IS_COMBAT_INITIALIZED = ANCHOR - 0x1F58L;
-
     public static final long IS_COMBAT_PAUSED = ANCHOR - 0x18F1L;
     public static final long IS_COMBAT_VIEW_OPEN = ANCHOR - 0x18F2L;
     // need to be updated end
 
+    // flags
+    public static final long IN_COMBAT = ANCHOR + 0x9CC30L;
+
+    // stats
     public static final long AMOUNT_OF_ENEMIES = ANCHOR - 0x8768L;
     public static final long SKILLPOINTS = ANCHOR - 0x8768L;
     public static final long TURN_COUNTER = ANCHOR + 0x1FB50L;
