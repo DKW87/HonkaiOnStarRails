b13573d4173c0e465c05434eeffc9f73cdc61124
DKW87 <dannykwant@gmail.com>
2025-06-04 08:25:41 +0200

    feat(CombatPtrChains): adds ptrChain class and moves from offset to chains

--- src/main/java/com/github/dkw87/honkaionstarrails/service/constant/CombatOffsets.java
+++ src/main/java/com/github/dkw87/honkaionstarrails/service/constant/CombatOffsets.java
@@ -15,30 +15,9 @@ public class CombatOffsets {
     public static final long IS_COMBAT_VIEW_OPEN = ANCHOR - 0x18F2L;
     // need to be updated end
 
-    public static final long TURN_COUNTER = ANCHOR + 0x1FB50L;
-
-    public static final long SKILLPOINTS = ANCHOR - 0x8768L;
-    public static final int[] SKILLPOINTS_PTR_CHAIN = {
-            0xF80,
-            0x88,
-            0x278,
-            0x38,
-            0x18,
-            0x10,
-            0x28,
-            0
-    };
-
     public static final long AMOUNT_OF_ENEMIES = ANCHOR - 0x8768L;
-    public static final int[] AMOUNT_OF_ENEMIES_PTR_CHAIN = {
-            0xF80,
-            0x88,
-            0x278,
-            0x48,
-            0x38,
-            0x54,
-            0
-    };
+    public static final long SKILLPOINTS = ANCHOR - 0x8768L;
+    public static final long TURN_COUNTER = ANCHOR + 0x1FB50L;
 
     private CombatOffsets() {}
 
---
+++ src/main/java/com/github/dkw87/honkaionstarrails/service/constant/CombatPtrChains.java
@@ -0,0 +1,11 @@
+package com.github.dkw87.honkaionstarrails.service.constant;
+
+public class CombatPtrChains {
+
+    public static final int[] AMOUNT_OF_ENEMIES = { 0xF80, 0x88, 0x278, 0x48, 0x38, 0x54, 0 };
+    public static final int[] SKILLPOINTS = { 0xF80, 0x88, 0x278, 0x38, 0x18, 0x10, 0x28, 0 };
+    public static final int[] TURN_COUNTER = { 0xF80, 0x88, 0x278, 0x38, 0x18, 0x10, 0x28, 0 };
+
+    private CombatPtrChains() {}
+
+}
