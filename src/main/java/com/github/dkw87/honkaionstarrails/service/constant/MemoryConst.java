178c2aebe3454ee0c6217a52d8ec9296bd42b89d
DKW87 <dannykwant@gmail.com>
2025-06-07 13:33:30 +0200

    feat(Constants): adds MemoryConst

---
+++ src/main/java/com/github/dkw87/honkaionstarrails/service/constant/MemoryConst.java
@@ -0,0 +1,8 @@
+package com.github.dkw87.honkaionstarrails.service.constant;
+
+public class MemoryConst {
+
+    public static final String GAME_ASSEMBLY_MODULE = "GameAssembly.dll";
+    public static final long ANCHOR = 0x43B4588L;
+
+}
