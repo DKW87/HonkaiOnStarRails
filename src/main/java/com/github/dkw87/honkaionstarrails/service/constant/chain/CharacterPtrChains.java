2e853e99877a9e3b372a86c68bac2315fbe6fb30
DKW87 <dannykwant@gmail.com>
2025-06-07 13:58:24 +0200

    feat(constants): adds offsets/chains for current and max health of active character

---
+++ src/main/java/com/github/dkw87/honkaionstarrails/service/constant/chain/CharacterPtrChains.java
@@ -0,0 +1,9 @@
+package com.github.dkw87.honkaionstarrails.service.constant.chain;
+
+public class CharacterPtrChains {
+
+    public static final int[] ACTIVE_CHAR_MAX_HEALTH = { 0xF80, 0x90, 0x28, 0x100, 0x278, 0x20, 0xC0, 0x28, 0x64 };
+    public static final int[] ACTIVE_CHAR_CURRENT_HEALTH = { 0xF80, 0x90, 0xA0, 0x18, 0x278, 0x20, 0xC0, 0x70, 0x64 };
+
+    private CharacterPtrChains() {}
+}
--- src/main/java/com/github/dkw87/honkaionstarrails/service/constant/offset/CharacterOffsets.java
+++ src/main/java/com/github/dkw87/honkaionstarrails/service/constant/offset/CharacterOffsets.java
@@ -6,6 +6,9 @@ public class CharacterOffsets {
 
     private static final long ANCHOR = MemoryConst.ANCHOR;
 
+    public static final long ACTIVE_CHAR_MAX_HEALTH = ANCHOR - 0x8768L;
+    public static final long ACTIVE_CHAR_CURRENT_HEALTH = ANCHOR - 0x8768L;
+
     private CharacterOffsets() {}
 
 }
