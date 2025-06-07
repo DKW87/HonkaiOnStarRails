77c0a92d0178729d814a2eff26290d18a4129b6f
DKW87 <dannykwant@gmail.com>
2025-06-07 13:37:17 +0200

    feat(Constants): adds CharacterOffsets

---
+++ src/main/java/com/github/dkw87/honkaionstarrails/service/constant/CharacterOffsets.java
@@ -0,0 +1,9 @@
+package com.github.dkw87.honkaionstarrails.service.constant;
+
+public class CharacterOffsets {
+
+    private static final long ANCHOR = MemoryConst.ANCHOR;
+
+    private CharacterOffsets() {}
+
+}
