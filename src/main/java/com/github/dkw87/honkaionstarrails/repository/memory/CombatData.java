14e3a6ba2b6e20229e66bbcc7f8ff5c6c8ae13ea
DKW87 <dannykwant@gmail.com>
2025-06-21 21:31:28 +0200

    feat(CombatData): replaces getters/setters with annotation

--- src/main/java/com/github/dkw87/honkaionstarrails/repository/memory/CombatData.java
+++ src/main/java/com/github/dkw87/honkaionstarrails/repository/memory/CombatData.java
@@ -1,7 +1,12 @@
 package com.github.dkw87.honkaionstarrails.repository.memory;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.awt.image.BufferedImage;
 
+@Getter
+@Setter
 public class CombatData {
 
     private volatile boolean inCombat;
@@ -12,55 +17,5 @@ public class CombatData {
     private volatile int lastAnalyzedTurn;
 
     private CombatData() {}
-
-    // getters
-    public boolean getInCombat() {
-        return inCombat;
-    }
-
-    public int getAmountOfEnemies() {
-        return amountOfEnemies;
-    }
-
-    public int getCurrentSkillpoints() {
-        return currentSkillpoints;
-    }
-
-    public int getTurn() {
-        return turn;
-    }
-
-    public BufferedImage getCurrentTurnImage() {
-        return currentTurnImage;
-    }
-
-    public int getLastAnalyzedTurn() {
-        return lastAnalyzedTurn;
-    }
-
-    // setters
-    public void setInCombat(boolean inCombat) {
-        this.inCombat = inCombat;
-    }
-
-    public void setAmountOfEnemies(int amountOfEnemies) {
-        this.amountOfEnemies = amountOfEnemies;
-    }
-
-    public void setCurrentSkillpoints(int currentSkillpoints) {
-        this.currentSkillpoints = currentSkillpoints;
-    }
-
-    public void setTurn(int turn) {
-        this.turn = turn;
-    }
-
-    public void setLastAnalyzedTurn(int lastAnalyzedTurn) {
-        this.lastAnalyzedTurn = lastAnalyzedTurn;
-    }
-
-    public void setCurrentTurnImage(BufferedImage currentTurnImage) {
-        this.currentTurnImage = currentTurnImage;
-    }
 
 }
