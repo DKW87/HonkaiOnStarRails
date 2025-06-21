b9458b145ff2c76f363174cb80a47ee342a99ee0
DKW87 <dannykwant@gmail.com>
2025-06-21 20:18:52 +0200

    refactor(CombatData): changes Atomic for volatile

--- src/main/java/com/github/dkw87/honkaionstarrails/repository/memory/CombatData.java
+++ src/main/java/com/github/dkw87/honkaionstarrails/repository/memory/CombatData.java
@@ -1,14 +1,11 @@
 package com.github.dkw87.honkaionstarrails.repository.memory;
 
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
-
 public class CombatData {
 
-    public static final AtomicBoolean IN_COMBAT =  new AtomicBoolean(false);
-    public static final AtomicInteger AMOUNT_OF_ENEMIES = new AtomicInteger(0);
-    public static final AtomicInteger AMOUNT_OF_SKILLPOINTS = new AtomicInteger(0);
-    public static final AtomicInteger TURN_COUNTER = new AtomicInteger(0);
+    public static volatile boolean IN_COMBAT;
+    public static volatile int AMOUNT_OF_ENEMIES;
+    public static volatile int AMOUNT_OF_SKILLPOINTS;
+    public static volatile int TURN_COUNTER;
 
     private CombatData() {}
 
