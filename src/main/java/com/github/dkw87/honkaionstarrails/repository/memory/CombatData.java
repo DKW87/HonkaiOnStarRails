5511a20a9fb67a1049dabf1fa12813124f7c16fa
DKW87 <dannykwant@gmail.com>
2025-06-21 20:34:44 +0200

    refactor(CombatData): encapsulation

--- src/main/java/com/github/dkw87/honkaionstarrails/repository/memory/CombatData.java
+++ src/main/java/com/github/dkw87/honkaionstarrails/repository/memory/CombatData.java
@@ -2,11 +2,45 @@ package com.github.dkw87.honkaionstarrails.repository.memory;
 
 public class CombatData {
 
-    public static volatile boolean IN_COMBAT;
-    public static volatile int AMOUNT_OF_ENEMIES;
-    public static volatile int AMOUNT_OF_SKILLPOINTS;
-    public static volatile int TURN_COUNTER;
+    private volatile boolean inCombat;
+    private volatile int amountOfEnemies;
+    private volatile int currentSkillpoints;
+    private volatile int turn;
 
     private CombatData() {}
+
+    // getters
+    public boolean getInCombat() {
+        return inCombat;
+    }
+
+    public int getAmountOfEnemies() {
+        return amountOfEnemies;
+    }
+
+    public int getCurrentSkillpoints() {
+        return currentSkillpoints;
+    }
+
+    public int getTurn() {
+        return turn;
+    }
+
+    // setters
+    public void setInCombat(boolean inCombat) {
+        this.inCombat = inCombat;
+    }
+
+    public void setAmountOfEnemies(int amountOfEnemies) {
+        this.amountOfEnemies = amountOfEnemies;
+    }
+
+    public void setCurrentSkillpoints(int currentSkillpoints) {
+        this.currentSkillpoints = currentSkillpoints;
+    }
+
+    public void setTurn(int turn) {
+        this.turn = turn;
+    }
 
 }
