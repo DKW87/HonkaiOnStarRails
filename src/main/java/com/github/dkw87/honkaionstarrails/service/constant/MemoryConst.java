694fd7cfa610cbc52fea6213f2884c86abfdf989
DKW87 <dannykwant@gmail.com>
2025-06-21 21:52:14 +0200

    feat(MemoryConst): adds private constructor

--- src/main/java/com/github/dkw87/honkaionstarrails/service/constant/MemoryConst.java
+++ src/main/java/com/github/dkw87/honkaionstarrails/service/constant/MemoryConst.java
@@ -5,4 +5,6 @@ public class MemoryConst {
     public static final String GAME_ASSEMBLY_MODULE = "GameAssembly.dll";
     public static final long ANCHOR = 0x43B4588L;
 
+    private MemoryConst() {}
+
 }
