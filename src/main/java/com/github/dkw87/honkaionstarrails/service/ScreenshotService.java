681adf63f9ae9a32aa35a65df55b5f29e68d2751
DKW87 <dannykwant@gmail.com>
2025-06-22 11:12:40 +0200

    feat(ScreenshotService): adds method to save images for debugging/development purposes

--- src/main/java/com/github/dkw87/honkaionstarrails/service/ScreenshotService.java
+++ src/main/java/com/github/dkw87/honkaionstarrails/service/ScreenshotService.java
@@ -3,15 +3,23 @@ package com.github.dkw87.honkaionstarrails.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.imageio.ImageIO;
 import java.awt.Robot;
 import java.awt.image.BufferedImage;
 import java.awt.Rectangle;
 import java.awt.Toolkit;
 import java.awt.AWTException;
+import java.io.File;
+import java.io.IOException;
 
 public class ScreenshotService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ScreenshotService.class);
+    private final File screenshotsFolder;
+
+    public ScreenshotService() {
+        screenshotsFolder = new File("screenshots");
+    }
 
     public BufferedImage takeScreenshot() {
         try {
@@ -25,6 +33,26 @@ public class ScreenshotService {
             LOGGER.error("Failed to create Robot: {}", e.getMessage());
         }
         return null;
+    }
+
+    public void saveImage(BufferedImage image, String fileName) {
+        if (image == null) {
+            LOGGER.error("Image is null");
+            return;
+        } else if (fileName == null) {
+            LOGGER.error("FileName is null");
+        }
+
+        if (!screenshotsFolder.exists()) {
+            screenshotsFolder.mkdirs();
+        }
+
+        try {
+            File outputFile = new File(screenshotsFolder, fileName);
+            ImageIO.write(image, "png", outputFile);
+        } catch (IOException e) {
+            LOGGER.error("Failed to save screenshot: {}", e.getMessage());
+        }
     }
 
 }
