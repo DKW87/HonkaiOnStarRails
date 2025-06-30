c3b6a7e71de31e49d0e9d58bbe18513dd6229463
DKW87 <dannykwant@gmail.com>
2025-06-30 19:39:26 +0200

    chore(CharacterPtrChains): comment

--- src/main/java/com/github/dkw87/honkaionstarrails/service/constant/chain/CharacterPtrChains.java
+++ src/main/java/com/github/dkw87/honkaionstarrails/service/constant/chain/CharacterPtrChains.java
@@ -1,5 +1,8 @@
 package com.github.dkw87.honkaionstarrails.service.constant.chain;
 
+/**
+ * The necessary steps to take in order to navigate to the data we need
+ */
 public class CharacterPtrChains {
 
     public static final int[] ACTIVE_CHAR_MAX_HEALTH = { 0xF80, 0xA0, 0x18, 0x278, 0x20, 0xC0, 0x28, 0x64 };
