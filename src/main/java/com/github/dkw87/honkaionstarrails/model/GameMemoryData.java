71d2dd46efc606c83b736af4ef6ad50fa4aee8b9
DKW87 <dannykwant@gmail.com>
2025-07-10 21:44:40 +0200

    refactor(GameMemoryData): separates utility into GameMemoryDataReaderUtil

--- src/main/java/com/github/dkw87/honkaionstarrails/model/GameMemoryData.java
+++ src/main/java/com/github/dkw87/honkaionstarrails/model/GameMemoryData.java
@@ -1,15 +1,16 @@
 package com.github.dkw87.honkaionstarrails.model;
 
 import com.github.dkw87.honkaionstarrails.model.enumeration.MemoryType;
-import com.github.dkw87.honkaionstarrails.service.MemoryReadingService;
+import com.github.dkw87.honkaionstarrails.model.utility.GameMemoryDataReaderUtil;
+import lombok.Getter;
 
+@Getter
 public class GameMemoryData {
-
-    private static final String MEMORY_MODULE = "GameAssembly.dll";
 
     private final Long offset;
     private final Integer[] ptrChain;
     private final MemoryType memoryType;
+    private final GameMemoryDataReaderUtil dataReaderUtil;
 
     public GameMemoryData(Long offset,
                           Integer[] ptrChain,
@@ -17,48 +18,11 @@ public class GameMemoryData {
         this.offset = offset;
         this.ptrChain = ptrChain;
         this.memoryType = memoryType;
+        this.dataReaderUtil = GameMemoryDataReaderUtil.getInstance();
     }
 
     public Object readFromMemory() {
-        return ptrChain == null
-                ? fromOffset()
-                : withPtrChain();
+        return dataReaderUtil.readFromMemory(this);
     }
-
-    private Object fromOffset() {
-        final MemoryReadingService memoryReadingService = MemoryReadingService.getInstance();
-        final Long module = memoryReadingService.getModuleBaseAddresses(MEMORY_MODULE);
-
-        return switch (this.memoryType) {
-            case BYTE -> memoryReadingService.readByteFromAddress(module + this.offset);
-            case INT -> memoryReadingService.readIntFromAddress(module + this.offset);
-            case LONG -> memoryReadingService.readLongFromAddress(module + this.offset);
-        };
-    }
-
-    private Object withPtrChain() {
-        final MemoryReadingService memoryReadingService = MemoryReadingService.getInstance();
-        final Long module = memoryReadingService.getModuleBaseAddresses(MEMORY_MODULE);
-
-        return switch (this.memoryType) {
-            case BYTE -> bytePtrChain(memoryReadingService, module);
-            case INT -> intPtrChain(memoryReadingService, module);
-            case LONG -> longPtrChain(memoryReadingService, module);
-        };
-    }
-
-    private Object bytePtrChain(MemoryReadingService memoryReadingService, Long module) {
-        return null; // TODO implement
-    }
-
-    private Object intPtrChain(MemoryReadingService memoryReadingService, Long module) {
-        final Long address = memoryReadingService.readLongFromAddress(module + this.offset);
-        return memoryReadingService.followPTRChain(address, this.ptrChain);
-    }
-
-    private Object longPtrChain(MemoryReadingService memoryReadingService, Long module) {
-        return null; // TODO implement
-    }
-
 
 }
---
+++ src/main/java/com/github/dkw87/honkaionstarrails/model/utility/GameMemoryDataReaderUtil.java
@@ -0,0 +1,61 @@
+package com.github.dkw87.honkaionstarrails.model.utility;
+
+import com.github.dkw87.honkaionstarrails.model.GameMemoryData;
+import com.github.dkw87.honkaionstarrails.service.MemoryReadingService;
+
+public class GameMemoryDataReaderUtil {
+
+    private static final String MEMORY_MODULE = "GameAssembly.dll";
+
+    private GameMemoryDataReaderUtil() {}
+
+    public Object readFromMemory(GameMemoryData data) {
+        return data.getPtrChain() == null
+                ? fromOffset(data)
+                : withPtrChain(data);
+    }
+
+    private Object fromOffset(GameMemoryData data) {
+        final MemoryReadingService mrService = MemoryReadingService.getInstance();
+        final Long module = mrService.getModuleBaseAddresses(MEMORY_MODULE);
+
+        return switch (data.getMemoryType()) {
+            case BYTE -> mrService.readByteFromAddress(module + data.getOffset());
+            case INT -> mrService.readIntFromAddress(module + data.getOffset());
+            case LONG -> mrService.readLongFromAddress(module + data.getOffset());
+        };
+    }
+
+    private Object withPtrChain(GameMemoryData data) {
+        final MemoryReadingService mrService = MemoryReadingService.getInstance();
+        final Long module = mrService.getModuleBaseAddresses(MEMORY_MODULE);
+
+        return switch (data.getMemoryType()) {
+            case BYTE -> bytePtrChain(mrService, module, data);
+            case INT -> intPtrChain(mrService, module, data);
+            case LONG -> longPtrChain(mrService, module, data);
+        };
+    }
+
+    private Object bytePtrChain(MemoryReadingService mrService, Long module, GameMemoryData data) {
+        return null; // TODO implement
+    }
+
+    private Object intPtrChain(MemoryReadingService mrService, Long module, GameMemoryData data) {
+        final Long address = mrService.readLongFromAddress(module + data.getOffset());
+        return mrService.followPTRChain(address, data.getPtrChain());
+    }
+
+    private Object longPtrChain(MemoryReadingService mrService, Long module, GameMemoryData data) {
+        return null; // TODO implement
+    }
+
+    public static GameMemoryDataReaderUtil getInstance() {
+        return SingletonHolder.INSTANCE;
+    }
+
+    private static class SingletonHolder {
+        private static final GameMemoryDataReaderUtil INSTANCE = new GameMemoryDataReaderUtil();
+    }
+
+}
